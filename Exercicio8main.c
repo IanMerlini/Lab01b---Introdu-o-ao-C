dd49dc85e43b15a4d606b340fe3a7661d473e05b
Ian Merlini Kaltbeitzer <114316654+IanMerlini@users.noreply.github.com>
2023-09-15 14:17:07 -0300

    Add files via upload

---
+++ Exercicio8main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void calcularTransposta(int matriz[][100], int linhas, int colunas, int transposta[][100]) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            transposta[j][i] = matriz[i][j];
+        }
+    }
+}
+
+int main() {
+    int linhas, colunas;
+    
+    printf("Digite o número de linhas da matriz: ");
+    scanf("%d", &linhas);
+    
+    printf("Digite o número de colunas da matriz: ");
+    scanf("%d", &colunas);
+    
+    int matriz[100][100], transposta[100][100];
+    
+    printf("Digite os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    
+    calcularTransposta(matriz, linhas, colunas, transposta);
+    
+    printf("Matriz transposta:\n");
+    for (int i = 0; i < colunas; i++) {
+        for (int j = 0; j < linhas; j++) {
+            printf("%d ", transposta[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
